17044b17718b53045a77fe3d0edabc4d174d9c5e
Lurey2 <Lurey2@users.noreply.github.com>
2025-04-22 15:41:34 +0000

    deploy: d99ff1b15fbf090ce7464718e63c450c29646465

--- chunk-6O5MPOYF.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var r=[{path:"",loadChildren:()=>import("./chunk-QXVJPI3C.js").then(e=>e.routes)},{path:"me",loadChildren:()=>import("./chunk-255RN5MD.js").then(e=>e.routes)},{path:"skills",loadChildren:()=>import("./chunk-REBYHDH3.js").then(e=>e.routes)}];export{r as routes};
